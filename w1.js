e563a0a842ceef9b7bce9d122fbacfb3a6207221
Shubham Gupta <Shubhamg836@gmail.com>
2019-05-07 12:24:39 +0530

    Update w1.js

--- w1.js
+++ w1.js
@@ -7,8 +7,8 @@ $(window).load(function() {
     $.ajax({
         url: "https://jsonplaceholder.typicode.com/posts/3"
     }).done(function(data) {
-        var resp=JSON.parse(data);
-        $('#cardContent').append('<div><br/>DATA FROM A WEB-API : '+resp.UserId+" "+resp.Title+" "+resp.Body+'</div>');
+        var resp=JSON.parse(JSON.stringify( data));
+        $('#cardContent').append('<div><br/>DATA FROM A WEB-API : '+data.UserId+" "+data.Title+" "+data.Body+'</div>');
         $("#loader").css("display", "none");
         $("#cardContent").css("display", "block");
     });
