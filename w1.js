08da1a34e5d8810ace60c776a607e5f37df45cb1
Shubham Gupta <Shubhamg836@gmail.com>
2019-05-07 14:07:45 +0530

    WidgetId

--- w1.js
+++ w1.js
@@ -8,8 +8,8 @@ $(function() {
         url: "https://jsonplaceholder.typicode.com/posts/3"
     }).done(function(data) {
         var resp=JSON.parse(JSON.stringify( data));
-        $('#cardContent').append('<div><br/>DATA FROM A WEB-API : '+resp.userId+" "+resp.title+" "+resp.body+'</div>');
-        $("#loader").css("display", "none");
-        $("#cardContent").css("display", "block");
+        $('#w1_cardPanel #cardContent').append('<div><br/>DATA FROM A WEB-API : '+resp.userId+" "+resp.title+" "+resp.body+'</div>');
+        $("#w1_cardPanel #loader").css("display", "none");
+        $("#w1_cardPanel #cardContent").css("display", "block");
     });
 });
